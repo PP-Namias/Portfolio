9aad100063b8f5bbcc4c75b89de894f810ea3f1f
Jhon Keneth Namias <jkrbn99@gmail.com>
2025-09-23 23:21:15 +0800

    Remove archived posts list from ArchivePanel
    Replaces the dynamic list of archived posts with a static message and link to current posts. Cleans up unused script and simplifies the component.

--- src/components/archived/ArchivePanel.svelte
+++ src/components/archived/ArchivePanel.svelte
@@ -1,12 +1,4 @@
-<script>
-  export let sortedPosts = [];
-</script>
-
 <div class="archive-panel-archived">
   <h2>Archive (Archived)</h2>
-  <ul>
-    {#each sortedPosts as post}
-      <li>{post.title}</li>
-    {/each}
-  </ul>
+  <p>This archive has been archived. For current posts, see <a href="/posts/">Posts</a>.</p>
 </div>
