2ddceacd883ffa2bc8067211ecac18215231e4c0
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2026-07-04 22:34:30 +0800

    fix(tailwind): EPIC-D-S05 Add src/lib/** to content array
    - Prevent Tailwind from purging classes in lib directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   darkMode: ['class'],
-  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     container: {
       center: true,
