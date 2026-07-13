edaef909ea1a38d5e5eef13dcbaaab00f4563b25
Jhon Keneth Namias <jkrbn99@gmail.com>
2026-07-13 23:24:28 +0800

    feat(portfolio-v2): update code collapsible, MDX, and component preview

--- portfolio-v2/src/components/mdx.tsx
+++ portfolio-v2/src/components/mdx.tsx
@@ -25,7 +25,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { Code } from "@/components/ui/typography"
 import {
   Tabs,
   TabsContent,
@@ -33,6 +32,7 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/base/ui/tabs"
+import { Code } from "@/components/base/ui/typography"
 import { CodeCollapsibleWrapper } from "@/components/code-collapsible-wrapper"
 import { ComponentSource } from "@/components/component-source"
 import { AutoTypeTable } from "@/features/doc/components/auto-type-table"
@@ -48,12 +48,12 @@ import { Heading } from "./heading"
 import { mdxCodeBlockComponents } from "./mdx-code-block"
 
 const components: MDXRemoteProps["components"] = {
-  h1: (props) => <Heading as="h1" {...(props as React.ComponentProps<"h1">)} />,
-  h2: (props) => <Heading as="h2" {...(props as React.ComponentProps<"h2">)} />,
-  h3: (props) => <Heading as="h3" {...(props as React.ComponentProps<"h3">)} />,
-  h4: (props) => <Heading as="h4" {...(props as React.ComponentProps<"h4">)} />,
-  h5: (props) => <Heading as="h5" {...(props as React.ComponentProps<"h5">)} />,
-  h6: (props) => <Heading as="h6" {...(props as React.ComponentProps<"h6">)} />,
+  h1: (props: React.ComponentProps<"h1">) => <Heading as="h1" {...props} />,
+  h2: (props: React.ComponentProps<"h2">) => <Heading as="h2" {...props} />,
+  h3: (props: React.ComponentProps<"h3">) => <Heading as="h3" {...props} />,
+  h4: (props: React.ComponentProps<"h4">) => <Heading as="h4" {...props} />,
+  h5: (props: React.ComponentProps<"h5">) => <Heading as="h5" {...props} />,
+  h6: (props: React.ComponentProps<"h6">) => <Heading as="h6" {...props} />,
   table: Table,
   thead: TableHeader,
   tbody: TableBody,
