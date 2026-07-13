8e134c317198b567060fe95ac73684b0ddb5621f
Jhon Keneth Namias <jkrbn99@gmail.com>
2026-07-13 23:23:54 +0800

    feat(portfolio-v2): update OG image route handlers

--- portfolio-v2/src/app/og/page.tsx
+++ portfolio-v2/src/app/og/page.tsx
@@ -1,10 +1,10 @@
-import { NamiasMarkIsometric } from "@/features/portfolio/components/namias-mark-isometric"
+import { ChanhDaiMarkIsometric } from "@/features/portfolio/components/chanhdai-mark-isometric"
 
 export default function Page() {
   return (
     <div className="max-w-screen overflow-x-clip">
       <div className="mx-auto flex h-screen flex-col justify-center md:max-w-3xl">
-        <NamiasMarkIsometric />
+        <ChanhDaiMarkIsometric />
       </div>
     </div>
   )
